1a5f23e64314f858b244deb04a9f36fc1c89665d
Bijaya Gautam <bijayagautam8@gmail.com>
2019-10-29 23:02:55 -0400

    Added EXERCISE 2: The Age Calculator

--- js/index.js
+++ js/index.js
@@ -8,9 +8,14 @@ let companyName = `TD Bank`;
 
 console.log(`You will be a ${jobTitle} in ${geographicLocation}, making ${annualSlary} for ${companyName}.`);
 
-
 //EXERCISE 2: The Age Calculator
 //Forgot how old someone is? Calculate it! Store the current year in a variable. Store their birth year in a variable. Calculate their age based on the stored values. Output them to the screen like so: "They are NN years old.", substituting the values.
+
+let currentYear = 2019;
+let birthYear = 1992;
+let age = currentYear - birthYear;
+
+console.log(`They are ${age} years old.`)
 
 //EXERCISE 3: The Lifetime Supply Calculator
 //Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more! Store your current age into a variable. Store a maximum age into a variable. Store an estimated amount per day (as a number). Calculate how many you would eat total for the rest of your life. Output the result to the screen like so: "You will need NN to last you until the ripe old age of X".
